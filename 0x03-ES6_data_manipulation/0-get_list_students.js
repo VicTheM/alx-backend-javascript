d4e1ca351625917dcd039d989eef82d6a0107639
Victory Chibuike <victorychibuike121@gmail.com>
2024-10-08 04:12:26 +0100

    corrected output

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -7,9 +7,9 @@
  * - `location` {string}: The location of the student.
  */
 export default function getListStudents() {
-  const obj1 = { id: 1, firstname: 'Guillaume', location: 'San Francisco' };
-  const obj2 = { id: 2, firstname: 'James', location: 'Columbia' };
-  const obj3 = { id: 5, firstname: 'Serena', location: 'San Francisco' };
-
-  return [obj1, obj2, obj3];
+  return [
+    { id: 1, firstname: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstname: 'James', location: 'Columbia' },
+    { id: 5, firstname: 'Serena', location: 'San Francisco' },
+  ];
 }
