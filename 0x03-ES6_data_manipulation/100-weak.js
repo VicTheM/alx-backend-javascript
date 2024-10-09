10c65da1b1efbf7db904f4cac8aa8d2477aff2be
Victory <victorychibuike121@gmail.com>
2024-10-09 01:29:54 +0100

    Update 100-weak.js

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,12 +1,12 @@
+const MAX_NUMBER_OF_QUERIES = 5;
 export const weakMap = new WeakMap();
-export const queryAPI = (endpoint) => {
-  if (weakMap.has(endpoint)) {
-    const calls = weakMap.get(endpoint);
-    weakMap.set(endpoint, calls + 1);
-    if (calls >= 5) {
-      throw new Error('Endpoint load is high');
-    }
-  } else {
-    weakMap.set(endpoint, 1);
+
+export function queryAPI(endpoint) {
+  if (!weakMap.has(endpoint)) {
+    weakMap.set(endpoint, 0);
   }
-};
+  weakMap.set(endpoint, weakMap.get(endpoint) + 1);
+  if (weakMap.get(endpoint) >= MAX_NUMBER_OF_QUERIES) {
+    throw new Error('Endpoint load is high');
+  }
+}
